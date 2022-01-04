3f5d15074e4ba000e05d3b75aff0c52ef24ab633
saitarun135 <nikhilnaidu473@gmail.com>
2022-01-04 15:35:24 +0530

    UC-9:- Performed validations on the setter method.

--- js_files/EmployeePayrollForm.js
+++ js_files/EmployeePayrollForm.js
@@ -1,7 +1,24 @@
-/** UC-8:- Ability to Set Event Listener on Salary Range to display appropriate value. */
-const salary = document.querySelector("#salary");
-const salaryOutput = document.querySelector(".salary-output");
-salaryOutput.textContent = salary.value;
-salary.addEventListener('input' , function(){
-    salaryOutput.textContent = salary.value;
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if (name.value.length == 0) {
+            textError.textContent = " ";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = " ";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+    });
+
 });
